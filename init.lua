b39b6de8f90b0af3627818574aa697a4b6c524bf
Lucy Kowal <lucy.kowal@nordstrom.com>
2025-02-18 11:54:41 -0800

    fix copilot chat

--- init.lua
+++ init.lua
@@ -1207,7 +1207,7 @@ require("lazy").setup({
 			and {
 				"CopilotC-Nvim/CopilotChat.nvim",
 				dependencies = {
-					{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+					{ "zbirenbaum/copilot.lua" }, -- or github/copilot.lua
 					{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 				},
 				build = "make tiktoken", -- Only on MacOS or Linux
