f046ca79c762a20d4c842c8be44b5d5b8d0b7ddf
lucykowal <72417420+lucykowal@users.noreply.github.com>
2025-04-19 23:32:18 -0700

    tmux nvim split mgmt

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -130,6 +130,24 @@ return {
       },
     },
   },
+  { -- smart splits
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
   { -- personal dev
     "lucykowal/comfychat.nvim",
     cond = vim.uv.os_uname().machine ~= "x86_64",
