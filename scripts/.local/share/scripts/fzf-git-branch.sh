fe916a1f8c737396db3e8b6e8d230bfa2457f18b
lucykowal <72417420+lucykowal@users.noreply.github.com>
2025-06-08 00:56:25 -0700

    fixes to git script

--- scripts/.local/share/scripts/fzf-git-branch.sh
+++ scripts/.local/share/scripts/fzf-git-branch.sh
@@ -7,11 +7,12 @@
 #   fzf --tmux 60%,border-native -m -1 -0 |\
 #   xargs tmux send-keys -l
 
-git status $? &>/dev/null || exit 1
-
-git branch --no-color --format="%(if)%(HEAD)%(then)*%(else)-%(end)|%(refname:lstrip=2)" |\
+git -C $1 status $? &>/dev/null || exit 1
+branch=$(git -C $1 branch --no-color --format="%(if)%(HEAD)%(then)*%(else)-%(end)|%(refname:lstrip=2)" |\
   fzf --tmux 60%,border-native -d '|' --with-nth='{1} {2}' \
   --accept-nth 2 --input-label-pos=bottom \
   --bind 'ctrl-b:execute(git branch -c {q})+reload<git branch --no-color --format="%(if)%(HEAD)%(then)*%(else)-%(end)|%(refname:lstrip=2)">,ctrl-d:execute(git branch -d {2})+reload<git branch --no-color --format="%(if)%(HEAD)%(then)*%(else)-%(end)|%(refname:lstrip=2)">' \
-  --input-label="C-b: create branch {q} | C-d: delete branch | Enter: checkout branch" |\
-  xargs git checkout -q
+  --input-label="C-b: create branch {q} | C-d: delete branch | Enter: checkout branch")
+
+# git checkout "$branch" &>/dev/null || exit 1
+echo "$branch"
