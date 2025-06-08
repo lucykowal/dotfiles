4e45cd54989aeee5dc78f0b0761000951b2c1f2f
lucykowal <72417420+lucykowal@users.noreply.github.com>
2025-06-08 00:24:00 -0700

    create git branch fzf script

---
+++ scripts/.local/share/scripts/fzf-git-branch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# tmux capture-pane -CJp -S -400 |\
+#   rg -oe '\$[^\d {(\[][A-Za-z_\d]*' |\
+#   awk '!seen[$0]++' |\
+#   tac |\
+#   fzf --tmux 60%,border-native -m -1 -0 |\
+#   xargs tmux send-keys -l
+
+git status $? &>/dev/null || exit 1
+
+git branch --no-color --format="%(if)%(HEAD)%(then)*%(else)-%(end)|%(refname:lstrip=2)" |\
+  fzf --tmux 60%,border-native -d '|' --with-nth='{1} {2}' \
+  --accept-nth=2 --input-label-pos=bottom \
+  --bind 'ctrl-b:execute(git branch -c {q})+reload<git branch --no-color --format="%(if)%(HEAD)%(then)*%(else)-%(end)|%(refname:lstrip=2)">,ctrl-d:execute(git branch -d {2})+reload<git branch --no-color --format="%(if)%(HEAD)%(then)*%(else)-%(end)|%(refname:lstrip=2)">' \
+  --input-label="C-b: create branch {q} | C-d: delete branch | Enter: checkout branch" |\
+  xargs git checkout
