6f1b01be2c2207600fd23785d33a2e460eed83e5
lucykowal <72417420+lucykowal@users.noreply.github.com>
2025-10-12 00:41:10 -0700

    fzf/zsh tweaks for fzf picker

--- scripts/.local/share/scripts/fzf-safari-history.sh
+++ scripts/.local/share/scripts/fzf-safari-history.sh
@@ -5,5 +5,5 @@ sqlite3 -list $HOME/Library/Safari/History.db \
   "select t.url from history_items as t \
   group by t.url \
   order by max(t.visit_count_score) desc limit 100;" \
-  | fzf -m --tmux=60%,border-native --no-sort --ansi \
+  | fzf -m --no-sort --ansi \
   | xargs open
