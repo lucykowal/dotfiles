8a87dc6740db10512989848d6336fa8a83b215a7
lucykowal <72417420+lucykowal@users.noreply.github.com>
2025-10-19 01:41:22 -0700

    add s/ buffer search

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,4 +1,51 @@
 -- fuzzy finder
+
+-- custom actions for fzf pickers:
+-- fzf over current buffer with line numbers
+local function fzf_current_buffer()
+  local sel_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local numbered_lines = {}
+  for i, line in ipairs(sel_lines) do
+    table.insert(numbered_lines, string.format("%4d: %s", i, line))
+  end
+
+  vim.fn["fzf#run"](vim.fn["fzf#wrap"]({
+    source = numbered_lines,
+    -- activate a search based on query
+    ["sink*"] = function(lines)
+      if #lines >= 2 then
+        -- extract selected line/# and query
+        local query = lines[1]
+        local selected_line = lines[2]
+        local line_num = tonumber(selected_line:match("^%s*(%d+):"))
+
+        if line_num then
+          -- go to the line
+          vim.api.nvim_win_set_cursor(0, { line_num, 0 })
+          vim.cmd("normal! zz")
+
+          -- if there was a query, use it as search pattern
+          if query and query ~= "" then
+            -- defer to allow cursor move to complete
+            vim.defer_fn(function()
+              -- escape special characters for literal search
+              local escaped_query = vim.fn.escape(query, [[\/.*$^~[]])
+              vim.fn.setreg("/", escaped_query)
+              vim.fn.histadd("/", escaped_query)
+              vim.o.hlsearch = true
+
+              -- do the search
+              vim.fn.search(escaped_query, "c")
+            end, 10)
+          end
+        end
+      end
+    end,
+    options = { "--prompt=Lines>", "--print-query" },
+  }))
+end
+
+-- config
 return {
   "junegunn/fzf",
   event = "VeryLazy",
@@ -27,6 +74,8 @@ return {
       }))
     end, { desc = "[S]earch [N]otifications" })
 
+    vim.keymap.set("n", "<leader>s/", fzf_current_buffer, { desc = "[S]earch current buffer [/]" })
+
     vim.g.fzf_layout = {
       window = { -- pop-up at bottom
         width = 1.0,
@@ -34,6 +83,22 @@ return {
         relative = false,
         yoffset = 1.0,
       },
+    }
+
+    vim.g.fzf_colors = {
+      fg = { "fg", "Normal" },
+      bg = { "bg", "Normal" },
+      hl = { "fg", "Comment" },
+      ["fg+"] = { "fg", "CursorLine", "CursorColumn", "Normal" },
+      ["bg+"] = { "bg", "CursorLine", "CursorColumn" },
+      ["hl+"] = { "fg", "Statement" },
+      info = { "fg", "PreProc" },
+      border = { "fg", "Ignore" },
+      prompt = { "fg", "Conditional" },
+      pointer = { "fg", "Exception" },
+      marker = { "fg", "Keyword" },
+      spinner = { "fg", "Label" },
+      header = { "fg", "Comment" },
     }
 
     -- keys:
